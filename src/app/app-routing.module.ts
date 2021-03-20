fc5e33ff8644c00f5e6fda8c243485066eda0868
Ugur Timurcin <utimurcin@outlook.com>
2021-03-20 22:10:25 +0300

    new routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { ProductAddForms1Component } from './components/product/product-add-forms1/product-add-forms1/product-add-forms1.component';
 import { ProductAddForms2Component } from './components/product/product-add-forms2/product-add-forms2.component';
 import { ProductComponent } from './components/product/product.component';
+import { LoginGuard } from './models/login.guard';
 
 const routes: Routes = [
   {path: 'products', component: ProductComponent},
   {path: '', redirectTo: 'products', pathMatch: 'full'},
-  {path:'products-add-1', component: ProductAddForms1Component},
+  {path:'products-add-1', component: ProductAddForms1Component, canActivate:[LoginGuard]},
   {path:'products-add-2', component: ProductAddForms2Component},
-  {path:'products/category/:categoryId', component: ProductComponent}
+  {path:'products/category/:categoryId', component: ProductComponent},
+  {path:'login', component: LoginComponent}
 ];
 
 @NgModule({
